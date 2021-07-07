4d0eb006095e3da248d21a7cb60b46c532c9cbe0
SitharaSS <sitharasherin6@gmail.com>
2021-07-07 11:36:26 +0530

    Anitha commit

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class Sample {
-
+	public static void username(){
+		System.out.println("username");
+	}
+	public static void main(String[] args) {
+		username();
+	}
 }
